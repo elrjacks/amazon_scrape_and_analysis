d53085436e67ceacb5751a43eb5f786b4027547f
elrjacks <eleni.jackson217@gmail.com>
2023-06-30 15:14:15 -0500

    Clean up

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def store_db(product_asin, product_name, product_price, product_ratings, product
 
     # create table
     curr.execute(
-        '''CREATE TABLE IF NOT EXISTS search_result (ASIN text, name text, price real, ratings text, ratings_num text, details_link text)''')
+        '''CREATE TABLE IF NOT EXISTS search_result (ASIN text, name text, price real, ratings integer, ratings_num text, details_link text)''')
     # insert data into a table
     curr.executemany(
         "INSERT INTO search_result (ASIN, name, price, ratings, ratings_num, details_link) VALUES (?,?,?,?,?,?)",
